0b30ebaedc2a3897ac4bd3cd17a57800aaccd08a
Gershon <gershbookey@gmail.com>
2023-05-28 11:43:14 +0300

    added filter buttons for vacations list; not yet added functions for the filters

--- frontend/src/Components/VacationsArea/AllVacations/AllVacations.tsx
+++ frontend/src/Components/VacationsArea/AllVacations/AllVacations.tsx
@@ -5,10 +5,15 @@ import VacationsCard from "../VacationsCard/VacationsCard";
 import "./AllVacations.css";
 import { useState, useEffect } from 'react';
 import { authStore } from "../../../Redux/AuthState";
+import Home from "../../HomeArea/Home/Home";
 
 function AllVacations(): JSX.Element {
 
-    const [vacations, setVacations] = useState<VacationModel[]>([])
+    const [allVacations, setAllVacations] = useState<VacationModel[]>([])
+    const [vacationsFollowing, setVacationsFollowing] = useState<VacationModel[]>()
+    const [notYetStarted, setNotYetStarted] = useState<VacationModel[]>()
+    const [activeVacations, setActiveVacations] = useState<VacationModel[]>()
+    const [showVacations, setShowVacations] = useState<VacationModel[]>()
     const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false)
     const navigate = useNavigate()
 
@@ -24,14 +29,48 @@ function AllVacations(): JSX.Element {
 
     useEffect(()=>{
         vacationsService.getAllVacations()
-            .then(vacations => setVacations(vacations))
-            .catch(err => alert(err))
+            .then(vacations => sortVacationsByDate(vacations))
+            .catch(err => console.log(err))
     },[])
+
+    function sortVacationsByDate(vacations: VacationModel[]) {
+        const sortedVacations = vacations.sort((a, b) => a.vacationStart.localeCompare(b.vacationStart))
+        setAllVacations(sortedVacations)
+        setShowVacations(sortedVacations)
+    }
+
+    function sortVacationsByFollowing() {
+
+    }
+
+    function showNotYetStartedVacations() {
+        // let now = new Date()
+        // const sortedVacations = allVacations.filter(v => v.vacationStart = now.getUTCDate().toString())
+        // console.log(sortedVacations)
+    }
+
+    function showActiveVacations() {
+
+    }
 
     return (
         <div className="AllVacations">
-            {isLoggedIn && <>{vacations.map(v => <VacationsCard key={v.vacationId} vacation={v}/>)}</>}
-            {!isLoggedIn && <>{navigate("/home")}</>}
+            {isLoggedIn && <>
+            <div className="FilterOptions">
+                <h3>Filter Options</h3>
+                <button>Only show vacations you are following</button>
+                <br />
+                <button onClick={showNotYetStartedVacations}>Only show vacations that have not yet started</button>
+                <br />
+                <button>Only show active vacations</button>
+            </div>
+            <br />
+            <div className="Vacations">
+                {showVacations.map(v => <VacationsCard key={v.vacationId} vacation={v}/>)}
+            </div>
+            </>}
+            {/* {!isLoggedIn && <>{navigate("/home")}</>} */}
+            {!isLoggedIn && <Home />}
 			
         </div>
     );
