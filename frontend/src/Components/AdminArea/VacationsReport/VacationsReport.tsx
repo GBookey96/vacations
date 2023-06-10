08c707f2ca0367fd7332739af7dab381db1871ad
Gershon <gershbookey@gmail.com>
2023-06-10 21:53:57 +0300

    added VacationsReport page with barchart showing followercount for all the vacations

--- frontend/src/Components/AdminArea/VacationsReport/VacationsReport.tsx
+++ frontend/src/Components/AdminArea/VacationsReport/VacationsReport.tsx
@@ -3,32 +3,33 @@ import "./VacationsReport.css";
 import vacationsService from "../../../Services/VacationsService";
 import followerService from "../../../Services/FollowerService";
 import { Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis } from "recharts";
+import VacationModel from "../../../Models/vacations-model";
 
 function VacationsReport(): JSX.Element {
     
-    // const [listOfVacations, setListOfVacations] = useState<[]>([])
-    // const [vacationsWithFollowers, setVacationsWithFollowers] = useState<[]>([])
+    const [vacations, setVacations] = useState<VacationModel[]>([])
 
-    // useEffect(()=>{
-    //     // vacationsService.listOfVacationsWithFollowerCount()
-    //     //     .then(vacations => {
-    //     //         setListOfVacations(vacations)
-    //     //         console.log(vacations)
-    //     //     })
-    //     //     .catch(err => console.log(err))
-    // },[])
+    useEffect(()=>{
+        vacationsService.getAllVacations()
+            .then(vacations => {
+                vacations.forEach(v => v.vacationDestination = v.vacationDestination.substring(0,10))
+                setVacations(vacations)
+            })
+            .catch(err => console.log(err))
+    },[])
 
     return (
         <div className="VacationsReport">
 			<h2>Vacations Report</h2>
-            {/* <BarChart width={1500} height={250} data={listOfVacations}>
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="vacationDestination" />
-                <YAxis dataKey="followerCount"/>
-                <Tooltip />
-                <Legend />
-                <Bar dataKey="pv" fill="#8884d8" />
-            </BarChart> */}
+            <div className="BarChartContainer">
+                <BarChart width={1200} height={250} data={vacations}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="vacationDestination" />
+                    <YAxis/>
+                    <Legend />
+                    <Bar dataKey="followerCount" fill="#374151" />
+                </BarChart>
+            </div>
         </div>
     );
 }
